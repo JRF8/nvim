ec880f8d318e56b5f8862751e9d9db2f32e21190
jesse <jesse.frazier.8@gmail.com>
2025-05-06 20:55:56 -0400

    add some more core options and keymaps

--- lua/jesse/core/options.lua
+++ lua/jesse/core/options.lua
@@ -1,11 +1,10 @@
--- alias
-
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 4
 vim.opt.smartindent = true
+vim.opt.autoindent = true
 
 -- disable auto comment next line
 vim.api.nvim_create_autocmd("FileType", {
@@ -14,3 +13,5 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.formatoptions:remove({ 'r', 'o' })
   end,
 })
+
+vim.opt.clipboard:append("unnamedplus")
