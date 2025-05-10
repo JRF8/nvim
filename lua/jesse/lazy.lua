8d057573ea42bde35f34f5069dab63bbb019a1f6
jesse <jesse.frazier.8@gmail.com>
2025-05-10 01:24:19 -0400

    add lsp, mason and autocompletion

--- lua/jesse/lazy.lua
+++ lua/jesse/lazy.lua
@@ -26,6 +26,7 @@ require("lazy").setup({
   spec = {
     -- import your plugins
     { import = "jesse.plugins" },
+    { import = "jesse.plugins.lsp" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- automatically check for plugin updates
---
+++ lua/jesse/plugins/lsp/mason-lspconfig.lua
@@ -0,0 +1,12 @@
+return {
+  "mason-org/mason-lspconfig.nvim",
+  dependencies = {
+    "mason-org/mason.nvim",
+    "neovim/nvim-lspconfig",
+  },
+
+  config = function()
+    require("mason-lspconfig").setup()
+  end
+
+}
